49dcf83714bb0f95c99bf2772bf6cb892e23d9d9
margaritaritik <margaritakuryaeva@gmail.com>
2022-12-22 20:23:54 +0700

    sunrice spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import BookSpinner from "./spiners/BookSpinner/BookSpinner";
+import SunSpinner from "./spiners/SunSpinner/SunSpinner";
 
 function App() {
   return (
     <div className="App">
         <BookSpinner></BookSpinner>
+        <SunSpinner></SunSpinner>
     </div>
   );
 }
