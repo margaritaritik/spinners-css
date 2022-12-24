e6a007ff7b096460807f8136cc4ede670edb0299
margaritaritik <margaritakuryaeva@gmail.com>
2022-12-24 22:01:11 +0700

    create heartbeat spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import BookSpinner from "./spiners/BookSpinner/BookSpinner";
 import SunSpinner from "./spiners/SunSpinner/SunSpinner";
 import HeartSpinner from "./spiners/HeartSpinner/HeartSpinner";
+import HeartBeatSpinner from "./spiners/HeartBeatSpinner/HeartBeatSpinner";
 
 function App() {
   return (
     <div className="App">
         <BookSpinner></BookSpinner>
         <SunSpinner></SunSpinner>
-        <HeartSpinner></HeartSpinner>
+        <div >
+            <HeartSpinner></HeartSpinner>
+            <HeartBeatSpinner></HeartBeatSpinner>
+        </div>
     </div>
   );
 }
